00cb363fc83b555886674ae1ac378387d96f4dba
iraklitbz <iraklitbz@gmail.com>
2020-05-28 19:14:11 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,77 @@
+<?php /* Template Name: home */ ?>
+<?php get_header(); ?>
+    <div id="primary" class="container">
+    <section class="gallery-v2 padding-y-xl">
+  <div class="container max-width-adaptive-lg clearfix">
+    <div class="margin-bottom-xl clearfix">
+      <div class="grid gap-sm">
+        <div class="col-6@lg offset-3@lg">
+          <div class="text-component line-height-lg">
+            <h1>Build & Deploy</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed tenetur rem quam nihil dolorum expedita maxime nisi recusandae sequi magni culpa fuga accusamus eveniet fugiat ipsum ab consequuntur.</p>
+
+            <p class="text-right"><a class="btn btn--subtle" href="#0">Read more</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <ul class="gallery-v2__grid grid gap-sm">
+      <li class="gallery-v2__item col-6@sm col-3@lg">
+
+        <div class="card-v2 radius-md">
+          <figure>
+            <img src="../../../app/assets/img/gallery-v2-img-1.jpg" alt="Image description">
+            <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-center">
+              <div class="text-md text-base@md">Card Title</div>
+              <div class="margin-top-xxxs text-sm opacity-70%">Label</div>
+            </figcaption>
+          </figure>
+        </div>
+      </li>
+
+      <li class="gallery-v2__item col-6@sm col-3@lg">
+
+        <div class="card-v2 radius-md">
+          <figure>
+            <img src="../../../app/assets/img/gallery-v2-img-2.jpg" alt="Image description">
+            <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-center">
+              <div class="text-md text-base@md">Card Title</div>
+              <div class="margin-top-xxxs text-sm opacity-70%">Label</div>
+            </figcaption>
+          </figure>
+        </div>
+      </li>
+
+      <li class="gallery-v2__item col-6@sm col-3@lg">
+
+        <div class="card-v2 radius-md">
+          <figure>
+            <img src="../../../app/assets/img/gallery-v2-img-3.jpg" alt="Image description">
+            <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-center">
+              <div class="text-md text-base@md">Card Title</div>
+              <div class="margin-top-xxxs text-sm opacity-70%">Label</div>
+            </figcaption>
+          </figure>
+        </div>
+      </li>
+
+      <li class="gallery-v2__item col-6@sm col-3@lg">
+
+        <div class="card-v2 radius-md">
+          <figure>
+            <img src="../../../app/assets/img/gallery-v2-img-4.jpg" alt="Image description">
+            <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-center">
+              <div class="text-md text-base@md">Card Title</div>
+              <div class="margin-top-xxxs text-sm opacity-70%">Label</div>
+            </figcaption>
+          </figure>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+    </div><!-- .content-area -->
+    <?php get_sidebar(); ?>
+    <?php get_footer(); ?>
