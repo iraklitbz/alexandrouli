81caa3d8e637ede325edc5e074040fa546e4b515
irakli Tavberidze <irakli.tavberidze@tuispain.com>
2022-11-25 17:02:14 +0100

    added amazon contact api

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -3,28 +3,59 @@
         <Header />
         <section class="thank-you py-20 lg:py-32">
             <div class="col-6@md">
-                <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                <form name="contact" method="POST">
                     <input class="hidden" type="hidden" name="bot-field" />
                     <div class="margin-bottom-sm">
                         <label class="form-label margin-bottom-xxs" for="contactName">Name</label>
-                        <input class="form-control width-100%" type="text" name="contactName" id="contactName" required>
+                        <input class="form-control width-100%" type="text" name="name" id="contactName" :value="name" required>
                     </div>
                 
                     <div class="margin-bottom-sm">
                         <label class="form-label margin-bottom-xxs" for="contactEmail">Email</label>
-                        <input class="form-control width-100%" type="email" name="contactEmail" id="contactEmail">
+                        <input class="form-control width-100%" type="email" name="email" :value="email" id="contactEmail">
                     </div>
                 
                     <div class="margin-bottom-sm">
                         <label class="form-label margin-bottom-xxs" for="contactMessage">Message</label>
-                        <textarea class="form-control width-100%" rows="9" name="contactMessage" id="contactMessage"></textarea>
+                        <textarea class="form-control width-100%" rows="9" name="message" :value="message" id="contactMessage"></textarea>
                     </div>
                     
                     <div class="text-right">
-                        <button class="btn btn--primary" type="submit">Submit</button>
+                        <button class="btn btn--primary" type="submit" @click="handleMail">Submit</button>
                     </div>
                 </form>
             </div>
         </section>
     </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        name: '',
+        email: '',
+        message: '',
+      }
+    },
+    methods: {
+      handleMail(e) {
+        e.preventDefault();
+        /*ENVIAR FORMULARIO*/
+        fetch('https://v5wxvhyj1i.execute-api.eu-west-3.amazonaws.com/contactoapi', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.message)
+        }).then(async (res) => {
+          if (res.status === 200) {
+            await setTimeout(() =>{
+              console.log(res);
+            },2000)
+          }
+        })
+      }
+    }
+  }
+</script>
