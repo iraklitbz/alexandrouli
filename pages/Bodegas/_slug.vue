f9b75e1c6d68a11bb6e54e49f971e7740942b202
irakli Tavberidze <irakli.tavberidze@tuispain.com>
2022-12-19 17:03:16 +0100

    added shopGrid

--- pages/Bodegas/_slug.vue
+++ pages/Bodegas/_slug.vue
@@ -6,18 +6,21 @@
                 <div class="relative z-[2] flex lg:w-1/2 xl:w-[calc(100%/12*5)]">
                     <div class="bg-floor p-6 shadow-[0_0.9px_1.25px_hsla(var(--color-contrast-higher)/0.025),0_3px_5px_hsla(var(--color-contrast-higher)/0.05),0_12px_20px_hsla(var(--color-contrast-higher)/0.09)] my-28 mx-0">
                         <h1 class="text-[2.125rem] mb-3">{{title}}</h1>
-                        <p class="text-contrast-medium leading-normal mb-4">
+                        <p class="text-contrast-medium text-lg leading-normal mb-4">
                             {{description}}
                         </p>
                     </div>
                 </div>
-
                 <figure class="absolute z-[1] w-full top-0 left-0 h-full lg:w-[calc(100%/12*10)]">
                     <img class="w-full h-full object-cover" :src="image" :alt="'Bodega ' + title + 'imagen'">
                 </figure>
                 </div>
             </div>
         </section>
+        <ShopGrid
+            class="mt-20"
+            :products="products"
+        />
     </div>
 </template>
 <script>
@@ -27,7 +30,8 @@ export default {
         return {
             title: '',
             description: '',
-            image: ''
+            image: '',
+            products: []
         }
     },
     async mounted () {
@@ -39,6 +43,13 @@ export default {
                 this.image = response.data.data[0].attributes.feature.data.attributes.formats.medium.url
             ))
         .catch(error => (this.error = error))
+        await axios
+        .get(process.env.strapiUrl + `/api/products?filters[bodegas]filters[title][$eq]=${this.$route.params.slug}&populate=*` )
+        .then(response => (
+            this.products = response.data.data
+        ))
+        .catch(error => (this.error = error))
+        
     }
 }
 </script>
